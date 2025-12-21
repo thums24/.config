26a70983b47f7bc6a5a9f51d2f8cbb37ae4b4797
thomas <th.dessolier@alumnos.upm.es>
2025-12-21 07:50:39 +0100

    21/12/2025

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -37,10 +37,9 @@ return {
       },
     }
 
-    -- Get capabilities from blink.cmp
     local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-    -- Helper function for root_dir patterns
+    -- Helper function for root_dir
     local function root_pattern(...)
       local patterns = { ... }
       return function(fname)
@@ -53,10 +52,12 @@ return {
       end
     end
 
-    -- LSP Attach - common keymaps for all servers
+    -- LSP Attach
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
         local map = function(keys, func, desc, mode)
           mode = mode or 'n'
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
@@ -68,7 +69,16 @@ return {
         map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
         map('gy', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype')
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        map('K', vim.lsp.buf.hover, 'Hover')
+
+        -- CUSTOM: Maj k (Shift+K) behavior
+        if client and client.name == 'jdtls' then
+          -- For Java: Go to Definition
+          map('K', vim.lsp.buf.definition, 'Go to Definition')
+        else
+          -- For others: Hover documentation
+          map('K', vim.lsp.buf.hover, 'Hover')
+        end
+
         map('gK', vim.lsp.buf.signature_help, 'Signature Help')
         map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
         map('<leader>cr', vim.lsp.buf.rename, '[C]ode [R]ename')
@@ -77,8 +87,7 @@ return {
         end, '[C]ode [F]ormat')
         map('gO', require('telescope.builtin').lsp_document_symbols, 'Document Symbols')
 
-        -- Document highlight
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        -- Highlight references
         if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -92,23 +101,17 @@ return {
             callback = vim.lsp.buf.clear_references,
           })
         end
-
-        -- Inlay hints toggle
-        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-          map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-          end, '[T]oggle Inlay [H]ints')
-        end
       end,
     })
 
     -- Server configurations
     local servers = {
+      -- 1. Added minimal jdtls here. Mason will install it automatically.
+      jdtls = {},
+
       lua_ls = {
         settings = {
-          Lua = {
-            completion = { callSnippet = 'Replace' },
-          },
+          Lua = { completion = { callSnippet = 'Replace' } },
         },
       },
       solidity_ls_nomicfoundation = {
@@ -160,26 +163,14 @@ return {
       },
       vtsls = {
         cmd = { 'vtsls', '--stdio' },
-        filetypes = {
-          'javascript',
-          'javascriptreact',
-          'javascript.jsx',
-          'typescript',
-          'typescriptreact',
-          'typescript.tsx',
-        },
+        filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx' },
         root_dir = root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git'),
         settings = {
           complete_function_calls = true,
           vtsls = {
             enableMoveToFileCodeAction = true,
             autoUseWorkspaceTsdk = true,
-            experimental = {
-              maxInlayHintLength = 30,
-              completion = {
-                enableServerSideFuzzyMatch = true,
-              },
-            },
+            experimental = { maxInlayHintLength = 30, completion = { enableServerSideFuzzyMatch = true } },
           },
           typescript = {
             updateImportsOnFileMove = { enabled = 'always' },
@@ -209,44 +200,28 @@ return {
       },
     }
 
-    -- TypeScript-specific on_attach
     local typescript_on_attach = function(client, bufnr)
       local opts = { buffer = bufnr, silent = true }
       vim.keymap.set('n', '<leader>co', function()
-        vim.lsp.buf.code_action {
-          apply = true,
-          context = { only = { 'source.organizeImports' }, diagnostics = {} },
-        }
+        vim.lsp.buf.code_action { apply = true, context = { only = { 'source.organizeImports' }, diagnostics = {} } }
       end, vim.tbl_extend('force', opts, { desc = 'Organize Imports' }))
       vim.keymap.set('n', '<leader>cM', function()
-        vim.lsp.buf.code_action {
-          apply = true,
-          context = { only = { 'source.addMissingImports.ts' }, diagnostics = {} },
-        }
+        vim.lsp.buf.code_action { apply = true, context = { only = { 'source.addMissingImports.ts' }, diagnostics = {} } }
       end, vim.tbl_extend('force', opts, { desc = 'Add missing imports' }))
       vim.keymap.set('n', '<leader>cu', function()
-        vim.lsp.buf.code_action {
-          apply = true,
-          context = { only = { 'source.removeUnused.ts' }, diagnostics = {} },
-        }
+        vim.lsp.buf.code_action { apply = true, context = { only = { 'source.removeUnused.ts' }, diagnostics = {} } }
       end, vim.tbl_extend('force', opts, { desc = 'Remove unused imports' }))
       vim.keymap.set('n', '<leader>cD', function()
-        vim.lsp.buf.code_action {
-          apply = true,
-          context = { only = { 'source.fixAll.ts' }, diagnostics = {} },
-        }
+        vim.lsp.buf.code_action { apply = true, context = { only = { 'source.fixAll.ts' }, diagnostics = {} } }
       end, vim.tbl_extend('force', opts, { desc = 'Fix all diagnostics' }))
     end
 
-    -- Extend vtsls config with on_attach
     servers.vtsls.on_attach = typescript_on_attach
 
-    -- Install tools
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, { 'stylua' })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-    -- Setup servers
     require('mason-lspconfig').setup {
       ensure_installed = {},
       automatic_installation = false,
